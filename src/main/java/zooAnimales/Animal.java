c28e212ab07f2a1d910da5fc1c6eca98c0645e0c
jeferson-rpo <98125058+jeferson-rpo@users.noreply.github.com>
2022-09-26 22:14:25 -0500

    cambio

--- src/main/java/zooAnimales/Animal.java
+++ src/main/java/zooAnimales/Animal.java
@@ -1,139 +1,140 @@
 package zooAnimales;
-
-import java.util.ArrayList;
-
-
 import gestion.Zona;
-
+import java.util.ArrayList;
 
 public class Animal {
 	private int totalAnimales;
-	 String nombre;
-	 int edad;
-	 String habitat;
-	 String genero;
-	private ArrayList<Zona> zonas= new ArrayList<>();
-	private static int totalMamiferos;
-	private static int totalAves;
-	private static int totalReptiles;
-	private static int totalPeces;
-	private static int totalAnfibios;
-	
-	public Animal() {
-		this(null,0,null,null);
-	}
-	
-	public Animal (String nombre, int edad,String habitat, String genero) {
-		this.nombre= nombre;
-		this.edad=edad;
-		this.habitat=habitat;
-		this.genero=genero;
-		setTotalAnimales(getTotalAnimales() + 1);
-		
-	}
-	public String movimiento() {
-		String a="desplazarse";
-		return a;
-	}
-	public static String totalPorTipo() {
-		return "Mamiferos: "+ totalMamiferos + "/nAves: " + totalAves+"/nReptiles: "+totalReptiles+
-				"/nPeces: "+totalPeces+"/nAnfibios:"+totalAnfibios;
-		}
+    private static int totalMamiferos;
+    private static int totalAves;
+    private static int totalReptiles;
+    private static int totalPeces;
+    private static int totalAnfibios;
 
-	public ArrayList<Zona> getZonas() {
-		return zonas;
-	}
+    public static int getTotalMamiferos() {
+        return totalMamiferos;
+    }
 
-	public void setZonas(ArrayList<Zona> zonas) {
-		this.zonas = zonas;
-	}
+    public static void setTotalMamiferos(int totalMamiferos) {
+        Animal.totalMamiferos = Animal.totalMamiferos + totalMamiferos;
+    }
 
-	public static int getTotalMamiferos() {
-		return totalMamiferos;
-	}
+    public static int getTotalAves() {
+        return totalAves;
+    }
 
-	public static void setTotalMamiferos(int totalMamiferos) {
-		Animal.totalMamiferos = Animal.totalMamiferos+ totalMamiferos ;
-	}
+    public static void setTotalAves(int totalAves) {
+        Animal.totalAves = Animal.totalAves + totalAves;
+    }
 
-	public static int getTotalAves() {
-		return totalAves;
-	}
+    public static int getTotalReptiles() {
+        return totalReptiles;
+    }
 
-	public static void setTotalAves(int totalAves) {
-		Animal.totalAves = Animal.totalAves+totalAves;
-	}
+    public static void setTotalReptiles(int totalReptiles) {
+        Animal.totalReptiles = Animal.totalReptiles + totalReptiles;
+    }
 
-	public static int getTotalReptiles() {
-		return totalReptiles;
-	}
+    public static int getTotalPeces() {
+        return totalPeces;
+    }
 
-	public static void setTotalReptiles(int totalReptiles) {
-		Animal.totalReptiles = Animal.totalReptiles+ totalReptiles;
-	}
+    public static void setTotalPeces(int totalPeces) {
+        Animal.totalPeces = Animal.totalPeces + totalPeces;
+    }
 
-	public static int getTotalPeces() {
-		return totalPeces;
-	}
+    public static int getTotalAnfibios() {
+        return totalAnfibios;
+    }
 
-	public static void setTotalPeces(int totalPeces) {
-		Animal.totalPeces = Animal.totalPeces +totalPeces;
-	}
+    public static void setTotalAnfibios(int totalAnfibios) {
+        Animal.totalAnfibios = Animal.totalAnfibios + totalAnfibios;
+    }
 
-	public static int getTotalAnfibios() {
-		return totalAnfibios;
-	}
+    private String nombre;
+    private int edad;
+    private String habitat;
+    private String genero;
+    private Zona zona;
 
-	public static void setTotalAnfibios(int totalAnfibios) {
-		Animal.totalAnfibios = Animal.totalAnfibios+ totalAnfibios;
-	}
-	public String getNombre() {
-    	return nombre;
-    	
-	}
- 
-	public void setNombre (String nombre) {
-    	this.nombre= nombre;
-	}
-	public String getHabitat() {
-		return habitat;
-	}
-	public void setHabitat(String habitat) {
-		this.habitat= habitat;
-	}
-	public int getEdad() {
-		return edad;
-	}
-	public void setEdad(int edad) {
-		this.edad= edad;
-	}
-	public String getGenero() {
-		return genero;
-	}
-	public void setGenero(String genero) {
-		this.genero= genero;
-	}
+    public Animal() {
+        this(null, 0, null, null);
+    }
 
-	public int getTotalAnimales() {
-		return totalAnimales;
-	}
+    public Animal(String nombre, int edad, String habitat, String genero) {
+        this.nombre = nombre;
+        this.edad = edad;
+        this.habitat = habitat;
+        this.genero = genero;
 
-	public void setTotalAnimales(int totalAnimales) {
-		this.totalAnimales = totalAnimales;
-	}
-	public String toString() {
-		if (this.zonas != null){
-			return "Mi nombre es " + this.getNombre() + ", tengo una edad de " + this.getEdad() + ", habito en " + this.getHabitat() + " y mi genero es " +
-				this.getGenero() + ", la zona en la que me ubico es " + this.getZonas() + ", en el " + Zona.getZoo().getNombre();
-		}
-		else {
-			return "Mi nombre es " + this.getNombre() + ", tengo una edad de " + this.getEdad() + ", habito en " + this.getHabitat() + " y mi genero es " +
-					this.getGenero();
-		}
-	}
+        totalAnimales++;
+    }
 
-	
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public int getEdad() {
+        return edad;
+    }
+
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
+
+    public String getHabitat() {
+        return habitat;
+    }
+
+    public void setHabitat(String habitat) {
+        this.habitat = habitat;
+    }
+
+    public String getGenero() {
+        return genero;
+    }
+
+    public void setGenero(String genero) {
+        this.genero = genero;
+    }
+
+    public String movimiento() {
+        return "desplazarse";
+    }
+
+    public static String totalPorTipo() {
+
+        return "Mamiferos: "+getTotalMamiferos()+"\n" +
+                "Aves: "+getTotalAves()+"\n" +
+                "Reptiles: "+getTotalReptiles()+"\n" +
+                "Peces: "+getTotalPeces()+"\n" +
+                "Anfibios: "+getTotalAnfibios();
+    }
+
+    @Override
+    public String toString() {
+        String nombreAnimal = nombre;
+        int edadAnimal = edad;
+        String habitatAnimal = habitat;
+        String generoAnimal = genero;
+        String zonaAnimal;
+        String nombreZoo;
+
+        if (zona != null) {
+            zonaAnimal = zona.getNombre();
+            nombreZoo = zona.getZoo().getNombre();
+
+            return "Mi nombre es " + nombreAnimal + ", tengo una edad de " + edadAnimal + ", habito en " +
+                    habitatAnimal + " y mi genero es" + generoAnimal + ", la zona en la que me ubico es " +
+                    zonaAnimal + ", en el " + nombreZoo;
+        } else {
+            return "Mi nombre es " + nombreAnimal + ", tengo una edad de " + edadAnimal +
+                    ", habito en " + habitatAnimal + " y mi genero es " + generoAnimal;
+        }
+    }
 }
-
 	
 
